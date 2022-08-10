ec8089509b971f28de4c136ebd9e313b3b4ced26
Jonathan Dias <jonathandiaas97@hotmail.com>
2022-08-10 15:03:11 -0300

    Added pizza info from JSON

--- script.js
+++ script.js
@@ -5,14 +5,32 @@ const sa = (el) => document.querySelectorAll(el);
 pizzaJson.map((item, index) =>{
     let pizzaItem = s('.models .pizza-item').cloneNode(true);
 
+    // Set new attribute with the index number
+    pizzaItem.setAttribute('data-key', index);
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
-    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price[0].value.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price[2].value.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
     
     pizzaItem.querySelector('a').addEventListener('click', (e)=>{
         // Block default action. The "a" element when clicked will not refresh the page, will not execute the href="".
         e.preventDefault();
+        // .target is the selected element('a'). | .closest get the closest element of the target and after that get the attribute data-key
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');        
+
+        // set pizzaJSON values at position X(key)
+        s('.pizzaBig img').src = pizzaJson[key].img;
+        s('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        s('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;  
+        
+        s('.pizzaInfo--size.selected').classList.remove('selected');
+        sa('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+            if (sizeIndex == 2) {
+                size.classList.add('selected');
+            };
+
+            size.querySelector('span').innerHTML = pizzaJson[key].price[sizeIndex].size;
+        });
 
         let pizzaWindowArea = s('.pizzaWindowArea');
 
